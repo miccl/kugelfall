538da244d40ebfa38e03b5310253ab12cd99ad87
StulleBrot <hertha_mcc@hotmail.de>
2016-05-11 19:04:46 +0200

    Adding first arduino code

---
+++ code/sensorwerterfassung/sensorwerterfassung.ino
@@ -0,0 +1,12 @@
+/**
+ * Sensorwerterfassung
+ */
+void setup() {
+  // put your setup code here, to run once:
+  // bla bla
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
